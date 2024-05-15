1793d7809b89ea1a650802a86d930c3d0db8b47a
Aarush Kashyap <aarush.kashyap21@gmail.com>
2024-05-15 15:56:43 +0530

    Created simple logo-name area

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,7 +5,13 @@ export default function Calculator() {
     <div className='bg-gray-800 text-white w-screen h-screen flex justify-center items-center'>
       <div className='main w-64 h-auto bg-green-800 rounded-2xl shadow-2xl border-4 border-black'>
         <div className="screen p-2">
-            <input type="text" className='text-green-600 w-full shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] placeholder-green-600 px-1 rounded-lg bg-gray-800 text-2xl pt-10 outline-none text-right' placeholder="0" />
+            <input type="text" className='text-green-600 w-full shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)]
+             placeholder-green-600 px-1 rounded-lg bg-gray-800 text-2xl pt-10 outline-none text-right' 
+             placeholder="0" />
+        </div>
+        
+        <div className='logo bg-green-800 bg-opacity-90 mb-2'>
+        <h1 className='text-gray-800 text-sm font-bold text-center'> Calculator </h1>
         </div>
       </div>
     </div>
